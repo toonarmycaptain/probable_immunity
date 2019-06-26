5babdac2dacc78caea2ca3a6bda62d6243543536
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-06-25 23:59:10 -0500

    Add test fixtures.

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from probable_immunity_app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app({
+        'TESTING': True,
+    })
+
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
