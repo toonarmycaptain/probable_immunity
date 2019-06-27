40ac3cef796e968db1234d616500e9b41bbf7447
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-06-27 15:20:33 -0500

    Add tests for measles_immunity/results.

--- tests/test_probable_immunity_app/test_probable_immunity_app.py
+++ tests/test_probable_immunity_app/test_probable_immunity_app.py
@@ -29,12 +29,35 @@ def test_measles_immunity_validate_input(client, app,
     assert message in response.data
 
 
+@pytest.mark.parametrize(
+    'request_data, response_status, probability',
+    [  # 0 shots
+        ({'birth_year': '1956', 'on_time_measles_vaccinations': '0'}, 200, b'1.0'),
+        ({'birth_year': '1957', 'on_time_measles_vaccinations': '0'}, 200, b'0.0'),
+        ({'birth_year': '1958', 'on_time_measles_vaccinations': '0'}, 200, b'0.0'),
+        ({'birth_year': '2011', 'on_time_measles_vaccinations': '0'}, 200, b'0.0'),
+        # 1 shot
+        ({'birth_year': '1957', 'on_time_measles_vaccinations': '1'}, 200, b'0.93'),
+        ({'birth_year': '1958', 'on_time_measles_vaccinations': '1'}, 200, b'0.93'),
+        ({'birth_year': '2011', 'on_time_measles_vaccinations': '1'}, 200, b'0.93'),
+        # 2 shots
+        ({'birth_year': '1957', 'on_time_measles_vaccinations': '2'}, 200, b'0.97'),
+        ({'birth_year': '1958', 'on_time_measles_vaccinations': '2'}, 200, b'0.97'),
+        ({'birth_year': '2011', 'on_time_measles_vaccinations': '2'}, 200, b'0.97'),
+        # >2 shots
+        ({'birth_year': '1957', 'on_time_measles_vaccinations': '3'}, 200, b'0.97'),
+        ({'birth_year': '1958', 'on_time_measles_vaccinations': '7'}, 200, b'0.97'),
+        ({'birth_year': '2011', 'on_time_measles_vaccinations': '12'}, 200, b'0.97'),
+    ])
+def test_measles_immunity_results(client, app,
+                                  request_data,
+                                  response_status, probability):
+    assert client.get('immunity_app/measles_immunity').status_code == 200
+    response = client.post(
+        'immunity_app/measles_immunity', data=request_data)
+    assert 'http://localhost/immunity_app/measles_immunity/results' == response.headers['Location']
 
-
-# def test_measles_immunity_results(client, app):
-#     assert client.post('immunity_app/measles_immunity/results').status_code == 200
-#     response = client.post(
-#         'immunity_app/measles_immunity', data={'birth_year': '2019',
-#                                                'on_time_measles_vaccinations': '1'}
-#     )
-#     assert 'http://localhost/immunity_app/measles_immunity/results' == response.headers['Location']
+    response = client.get('http://localhost/immunity_app/measles_immunity/results', follow_redirects=True)
+    assert response.status_code == response_status
+    # Use probability of immunity to test response content.
+    assert probability in response.data
