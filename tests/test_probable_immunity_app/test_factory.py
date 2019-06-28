d1a59bc38e3eaa8dc8090c1c42bf4d6a9ff440ac
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-06-28 15:22:56 -0500

    PEP8 whitespace.

--- tests/test_probable_immunity_app/test_factory.py
+++ tests/test_probable_immunity_app/test_factory.py
@@ -1,8 +1,10 @@
 from probable_immunity_app import about_text_string, create_app
 
+
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
+
 
 def test_about_text(client):
     response = client.get('/about_text')
