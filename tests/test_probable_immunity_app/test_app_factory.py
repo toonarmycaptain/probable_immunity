9b722694092b0f54116d2a74181bdf99d58af939
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-07-04 13:40:26 -0500

    Add tests for create_app, ensuring OSError is passed over.

--- tests/test_probable_immunity_app/test_app_factory.py
+++ tests/test_probable_immunity_app/test_app_factory.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+
+import flask
+
+import probable_immunity_web_app.app_factory as app_factory
+
 from probable_immunity_web_app.app_factory import about_text_string, create_app
 
 
@@ -9,3 +15,20 @@ def test_config():
 def test_about_text(client):
     response = client.get('/about_text')
     assert response.data == about_text_string
+
+
+def test_create_app():
+    app = create_app()
+    assert isinstance(app, flask.app.Flask)
+    assert app.name == 'probable_immunity_web_app.app_factory'
+
+
+def test_create_app_passing_os_error(monkeypatch):
+    def mocked_mkdir(app_instance_path, parents, exist_ok):
+        if not isinstance(app_instance_path, Path) and parents is True and exist_ok is True:
+            raise ValueError
+        raise OSError
+
+    monkeypatch.setattr(app_factory.Path, 'mkdir', mocked_mkdir)
+
+    assert create_app()
