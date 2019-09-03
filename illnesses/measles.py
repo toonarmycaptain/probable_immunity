aca7534cd3be0a309d7afc0ca9ccd14dfce8c692
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-09-03 11:33:56 -0500

    Configure measles to return list of templates rather than HTML.

--- illnesses/measles.py
+++ illnesses/measles.py
@@ -1,6 +1,5 @@
 from typing import Dict
 
-from flask import Markup
 """
 CDC Presumptive evidence of immunity: https://www.cdc.gov/vaccines/pubs/surv-manual/chpt07-measles.html
 
@@ -33,28 +32,19 @@ rec_shots_under_6 = 2
 
 conferred_immunity = 1
 
-messages = {'pre_1957_message': ("According to the CDC, you are likely immune to measles due to "
-                                 "childhood exposure.<br>"
-                                 "NB: \"Birth before 1957 provides only "
-                                 "presumptive evidence for measles, mumps, and rubella. Before "
-                                 "vaccines were available, nearly everyone was infected with "
-                                 "measles, mumps, and rubella viruses during childhood. The "
-                                 "majority of people born before 1957 are likely to have been "
-                                 "infected naturally and therefore are presumed to be protected "
-                                 "against measles, mumps, and rubella. Healthcare personnel born "
-                                 "before 1957 without laboratory evidence of immunity or disease "
-                                 "should consider getting two doses of MMR vaccine.\" "
-                                 "- <a href='https://www.cdc.gov/vaccines/vpd/mmr/public/index.html'>"
-                                 "CDC - Measles, Mumps, and Rubella (MMR) Vaccination: What Everyone Should Know</a>"),
-            'has_immunisations': ("This means you have a statistical probability of being immune "
-                                  "to measles if you are exposed. The closer to 1.0, the more "
-                                  "likely you are immune."),  # TODO make better message
-            'greater_than_two_shots_before_age_six_message': ("Data not available for more than 2 "
-                                                              "shots before age 6."),
-            'no_immunisations': ("You are unlikely to have any immunity to measles, if you are "
-                                 "exposed, you are very likely to be infected.")
-            }
-
+"""
+messages:   'pre_1957_message': CDC explanation of assumed immunity due to exposure before vaccines.
+                ref: https://www.cdc.gov/vaccines/vpd/mmr/public/index.html
+                                
+            'has_immunisations': Correct immunisations.  # TODO make better message
+            
+            'greater_than_two_shots_before_age_six_message': Note about data being unavailable for 
+                more than 2 shots before age 6. # TODO add caveat about meeting minimum 
+                requirements, likely immune. 
+                
+                                                             
+            'no_immunisations': Unlikely to have any immunity.
+"""
 # shots before 6 years
 shots_under_6_immunity = {
     1: 0.93,
@@ -67,19 +57,21 @@ def immunity(birth_year=None, on_time_measles_vaccinations: int = None) -> Dict:
     Takes year of birth, number of shots before age 6, and provides an
     estimated probability of being immune to measles if exposed.
 
+    Returns a float probability, and a list of content templates.
+
     :param birth_year: int or None
     :param on_time_measles_vaccinations: int or None
-    :return: Dict {'probability_of_measles_immunity': float, 'measles_message': str}
+    :return: Dict {'probability_of_measles_immunity': float, 'content_templates': List(str)}
     """
     # Set defaults:
-    probability, message = 0.0, messages['no_immunisations']
+    probability, messages = 0.0, ['no_immunisations']
     if birth_year < 1957:
-        probability, message = 1.0, messages['pre_1957_message']
+        probability, messages = 1.0, ['pre_1957_message']
     elif on_time_measles_vaccinations:
         if on_time_measles_vaccinations <= 2:
-            probability, message = shots_under_6_immunity[on_time_measles_vaccinations], messages['has_immunisations']
+            probability, messages = shots_under_6_immunity[on_time_measles_vaccinations], ['has_immunisations']
         if on_time_measles_vaccinations > 2:
-            probability, message = shots_under_6_immunity[2], messages['greater_than_two_shots_before_age_six_message']
-    return {'probability_of_measles_immunity': probability, 'measles_message': Markup(message)}
+            probability, messages = shots_under_6_immunity[2], ['greater_than_two_shots_before_age_six_message']
+    return {'probability_of_measles_immunity': probability, 'content_templates': messages}
 
 # need case where shots after age 6
--- tests/test_illnesses/test_measles.py
+++ tests/test_illnesses/test_measles.py
@@ -1,21 +1,20 @@
 import pytest
 
 from illnesses.measles import (immunity,
-                               messages,
                                )
 
 
 class TestImmunity:
     @pytest.mark.parametrize('args, returned_dict',
                              [  # pre 1957 birth year, no shots supplied
-                                 ((1900,), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
-                                 ((1956,), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
-                                 ((1956, None), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
+                                 ((1900,), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
+                                 ((1956,), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
+                                 ((1956, None), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
                                  # pre-1957 birth year, number of shots 0 -> >2
-                                 ((1900, 0), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
-                                 ((1956, 1), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
-                                 ((1900, 2), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
-                                 ((1956, 3), {'probability_of_measles_immunity': 1.0, 'measles_message': messages['pre_1957_message']}),
+                                 ((1900, 0), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
+                                 ((1956, 1), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
+                                 ((1900, 2), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
+                                 ((1956, 3), {'probability_of_measles_immunity': 1.0, 'content_templates': ['pre_1957_message']}),
                              ])
     def test_immunity_pre_1957(self, args, returned_dict):
         assert immunity(*args) == returned_dict
@@ -23,24 +22,24 @@ class TestImmunity:
 
     @pytest.mark.parametrize('args, returned_dict',
                              [  # 0 shots
-                                 ((1957,), {'probability_of_measles_immunity': 0.0, 'measles_message': messages['no_immunisations']}),
-                                 ((1958,), {'probability_of_measles_immunity': 0.0, 'measles_message': messages['no_immunisations']}),
-                                 ((2011,), {'probability_of_measles_immunity': 0.0, 'measles_message': messages['no_immunisations']}),
-                                 ((1957, 0), {'probability_of_measles_immunity': 0.0, 'measles_message': messages['no_immunisations']}),
-                                 ((1958, 0), {'probability_of_measles_immunity': 0.0, 'measles_message': messages['no_immunisations']}),
-                                 ((2011, 0), {'probability_of_measles_immunity': 0.0, 'measles_message': messages['no_immunisations']}),
+                                 ((1957,), {'probability_of_measles_immunity': 0.0, 'content_templates': ['no_immunisations']}),
+                                 ((1958,), {'probability_of_measles_immunity': 0.0, 'content_templates': ['no_immunisations']}),
+                                 ((2011,), {'probability_of_measles_immunity': 0.0, 'content_templates': ['no_immunisations']}),
+                                 ((1957, 0), {'probability_of_measles_immunity': 0.0, 'content_templates': ['no_immunisations']}),
+                                 ((1958, 0), {'probability_of_measles_immunity': 0.0, 'content_templates': ['no_immunisations']}),
+                                 ((2011, 0), {'probability_of_measles_immunity': 0.0, 'content_templates': ['no_immunisations']}),
                                  # 1 shot
-                                 ((1957, 1), {'probability_of_measles_immunity': 0.93, 'measles_message': messages['has_immunisations']}),
-                                 ((1958, 1), {'probability_of_measles_immunity': 0.93, 'measles_message': messages['has_immunisations']}),
-                                 ((2011, 1), {'probability_of_measles_immunity': 0.93, 'measles_message': messages['has_immunisations']}),
+                                 ((1957, 1), {'probability_of_measles_immunity': 0.93, 'content_templates': ['has_immunisations']}),
+                                 ((1958, 1), {'probability_of_measles_immunity': 0.93, 'content_templates': ['has_immunisations']}),
+                                 ((2011, 1), {'probability_of_measles_immunity': 0.93, 'content_templates': ['has_immunisations']}),
                                  # 2 shots
-                                 ((1957, 2), {'probability_of_measles_immunity': 0.97, 'measles_message': messages['has_immunisations']}),
-                                 ((1958, 2), {'probability_of_measles_immunity': 0.97, 'measles_message': messages['has_immunisations']}),
-                                 ((2011, 2), {'probability_of_measles_immunity': 0.97, 'measles_message': messages['has_immunisations']}),
+                                 ((1957, 2), {'probability_of_measles_immunity': 0.97, 'content_templates': ['has_immunisations']}),
+                                 ((1958, 2), {'probability_of_measles_immunity': 0.97, 'content_templates': ['has_immunisations']}),
+                                 ((2011, 2), {'probability_of_measles_immunity': 0.97, 'content_templates': ['has_immunisations']}),
                                  # >2 shots
-                                 ((1957, 3), {'probability_of_measles_immunity': 0.97, 'measles_message': messages['greater_than_two_shots_before_age_six_message']}),
-                                 ((1958, 7), {'probability_of_measles_immunity': 0.97, 'measles_message': messages['greater_than_two_shots_before_age_six_message']}),
-                                 ((2011, 12), {'probability_of_measles_immunity': 0.97, 'measles_message': messages['greater_than_two_shots_before_age_six_message']}),
+                                 ((1957, 3), {'probability_of_measles_immunity': 0.97, 'content_templates': ['greater_than_two_shots_before_age_six_message']}),
+                                 ((1958, 7), {'probability_of_measles_immunity': 0.97, 'content_templates': ['greater_than_two_shots_before_age_six_message']}),
+                                 ((2011, 12), {'probability_of_measles_immunity': 0.97, 'content_templates': ['greater_than_two_shots_before_age_six_message']}),
                              ])
     def test_immunity_post_1957(self, args, returned_dict):
         assert immunity(*args) == returned_dict
