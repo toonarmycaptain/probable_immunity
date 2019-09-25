7527daaded281f2e6995c6217271081fdc9d1d6e
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-09-25 14:41:49 -0500

    Add form objects, functions to extract form data into dicts.

---
+++ probable_immunity_web_app/forms/custom_validators.py
@@ -0,0 +1,58 @@
+from typing import Type
+
+from flask_wtf import FlaskForm
+from wtforms import ValidationError
+
+
+class Year(object):
+    """
+    Custom validator for year integer.
+    ...
+
+    Attributes:
+    ----------
+    min_year : int
+        Minimum year.
+    max_year : int
+        Maximum year.
+    digits : int
+        Length of value required. eg 27 = 2, 20293 = 5. Default: 4
+    message : str
+        Error message on validation failure.
+
+
+    """
+
+    def __init__(self, min_year: int = None,
+                 max_year: int = None,
+                 digits: int = 4,
+                 message=None,
+                 ):
+        """
+        :param min_year: int, the minimum year
+        :param max_year: int, the maximum year.
+        :param digits: int, length of value required, defaults to 4.
+        :param message: str, error message on validation failure.
+        """
+        self.min = min_year
+        self.max = max_year
+        self.num_digits = digits
+        if not message:
+            message = f'Field must be a {digits} digit year with a value between {min_year} and  {max_year}.'
+        self.message = message
+
+
+    def __call__(self, form: Type[FlaskForm], field):
+        """
+        :param form: Flask-WTF WTForm object
+        :param field: The field to be validated.
+        :return: None
+        :raises: ValidationError
+        """
+        # Cast to int if value exists and is string, otherwise assign 0, which will fail num_digits !=0.
+        value = field.data and int(field.data) or 0
+        if ((self.min is not None and value < self.min)
+                or (self.max is not None and value > self.max)
+                or (self.num_digits is not None and self.num_digits != len(str(value)))
+                or (not isinstance(value, int))):
+            raise ValidationError(self.message)
---
+++ probable_immunity_web_app/forms/immunity_data_entry_form.py
@@ -0,0 +1,31 @@
+"""Main immunity data entry form. Container for illness sub-forms."""
+
+import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import (FormField,
+                     validators,
+                     SubmitField)
+from wtforms.fields.html5 import IntegerField
+
+from probable_immunity_web_app.illness_config import illnesses
+from probable_immunity_web_app.forms import custom_validators
+
+current_year: int = datetime.date.today().year  # Returns 4 digit year.
+
+
+class ImmunityDataEntryForm(FlaskForm):
+    birth_year = IntegerField(
+        'Birth year:',
+        [
+            custom_validators.Year(
+                min_year=0,
+                max_year=current_year,
+                message=f'Birth year must be a 4 digit integer less than {current_year}.'),
+            validators.InputRequired(message='Birth year required.'),
+        ],
+    )
+
+    measles = FormField(illnesses.measles.form)
+
+    submit = SubmitField('Submit')
