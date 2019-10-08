5a68ff8b64e7e4372f3879f33e74e5540279fc3c
toonarmycaptain <toonarmycaptain@hotmail.com>
2019-10-08 00:09:08 -0500

    Add fixture taking a list of illness objects to test different configs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from probable_immunity_web_app.app_factory import create_app
+from probable_immunity_web_app.illnesses import Mumps, Measles
 
 
 @pytest.fixture
@@ -8,6 +9,7 @@ def app():
     app = create_app({
         'TESTING': True,
         'WTF_CSRF_ENABLED': False,
+        'ILLNESS_LIST': [Measles, Mumps],
     })
 
     yield app
@@ -21,3 +23,16 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+@pytest.fixture
+def app_specific_illnesses():
+    def _test_illnesses(illnesses):
+        app = create_app({
+            'TESTING': True,
+            'WTF_CSRF_ENABLED': False,
+            'ILLNESS_LIST': illnesses,
+        })
+        return app
+
+    return _test_illnesses
